4158e0cf887657450a1f1115f689e4340324cf3e
himank jain <himankjain@yahoo.in>
2024-04-20 16:39:46 +0530

    removed comments

--- packages/chain/src/spyMessages.ts
+++ packages/chain/src/spyMessages.ts
@@ -7,7 +7,7 @@ import { Field } from "o1js";
 
 @runtimeModule()
 export class SpyMessages extends RuntimeModule<Record<string, never>> {
-  //Note: Somehow its not working if fetched inside the method
+  
   @state() public agents = StateMap.from<UInt64, AgentDetails>(
     UInt64,
     AgentDetails
@@ -15,14 +15,13 @@ export class SpyMessages extends RuntimeModule<Record<string, never>> {
 
 
   @runtimeMethod()
-  // public addMessage(message: Message): void {
+ 
   public addMessage(agentId: UInt64, securityCode: SecurityCode, messageContent: MessageStruct, messageNo: UInt64): void {
     const someAgent = this.agents.get(UInt64.from(agentId))
     //check if agent exists
     assert(someAgent.isSome, "Agent code is present")
     const agent = new AgentDetails(someAgent.value)
     //check if security code matches
-    // const agentDetails = new AgentDetails({...someAgent.value})
     assert(securityCode.char1.equals(agent.securityCode1), "Secuity Code 1 is wrong")
     assert(securityCode.char2.equals(agent.securityCode2), "Secuity Code 2 is wrong")
     //check if message no is greater than current  
